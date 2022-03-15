969f5875861b22b292651816b33fc4889a492a10
marioshafiek <58153069+marioshafiek@users.noreply.github.com>
2022-03-15 19:12:53 +0200

    Make sure that test cases for ceaser all true

--- startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
+++ startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
@@ -2,25 +2,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace SecurityLibrary
+using System.Threading.Tasks;namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
-        public string Encrypt(string plainText, int key)
+        static string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static char[] letter = alpha.ToCharArray(); public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            string x = "";
+            char[] PL = plainText.ToUpper().ToCharArray();
+            for (int i = 0; i < PL.Length; i++)
+            {
+                for (int j = 0; j < letter.Length; j++)
+                {
+                    if (PL[i] == letter[j])
+                    {
+                        x += letter[(j + key) % 26];
+                        break;
+                    }
+                }
+            }
+            return x;
         }
-
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            char[] CT = cipherText.ToUpper().ToCharArray();
+            string x = "";
+            for (int i = 0; i < CT.Length; i++)
+            {
+                for (int j = 0; j < letter.Length; j++)
+                {
+                    if (CT[i] == letter[j])
+                    {
+                        int res = j - key;
+                        if (res < 0)
+                            res = res + 26;
+                        x += letter[res];
+                        break;
+                    }
+                }
+            }
+            return x;
         }
-
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            char[] CT = cipherText.ToUpper().ToCharArray();
+            int cipher = 0;
+            int plain = 0; for (int i = 0; i < letter.Length; i++)
+            {
+                if (CT[0] == letter[i])
+                {
+                    cipher = i;
+                    break;
+                }
+            }
+            char[] PT = plainText.ToUpper().ToCharArray();
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (PT[0] == letter[i])
+                {
+                    plain = i;
+                    break;
+                }
+            }
+            if (cipher < plain)
+                return (cipher - plain) + 26;
+            else
+                return cipher - plain;
         }
     }
 }
+
